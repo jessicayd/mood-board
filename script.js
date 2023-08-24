cfc04b8e750f8e8c2cbfcd6adc6988f3f7474eb6
Jessica Dong <jessicad.7373@gmail.com>
2023-08-24 15:59:38 -0400

    allows color input

--- script.js
+++ script.js
@@ -28,9 +28,29 @@ function createBoard() {
             if (i == 0) box.classList.add("firstRow");
             if (i == 11) box.classList.add("lastRow");
             box.classList.add("box");
+            box.id = `${i}${j}`;
             boxesContainer.append(box);
         }
     }
 }
 
-createBoard();
+function getHex() {
+    const form = document.getElementById('numberForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+
+        const hexInput = document.getElementById('hexInput');
+        const dateInput = document.getElementById('dateInput');
+        const date = dateInput.value.split(',');
+        
+        const currBox = document.getElementById(`${date[0]}${date[1]}`);
+        currBox.style.backgroundColor = `#${hexInput.value}`;
+        console.log("hi")
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    createBoard();
+    getHex();
+})
