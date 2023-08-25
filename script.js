64a78088d77f77c3187a1c3bf9b9dc3b876578ea
Jessica Dong <jessicad.7373@gmail.com>
2023-08-25 01:47:37 -0400

    making boxes clickable

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ const overlay = document.getElementById('overlay');
 
 function createBoard() {
     document.getElementById("date").innerHTML = `${monthNames[currDate.getMonth()]} ${currDate.getDate()}, ${year}`;
-    console.log(currDate)
     document.getElementById("datePopup").valueAsDate = new Date(year, month, day);
-    
 
     const daysContainer = document.getElementById("days");
     for (let i = 1; i <= 31; i++) {
@@ -39,38 +37,94 @@ function createBoard() {
             if (i == 11) box.classList.add("lastRow");
             box.classList.add("box");
             box.id = `${i}${j}`;
+
+            box.addEventListener('click', function() {
+                document.getElementById("datePopup").valueAsDate = new Date(year, i, j + 1);
+                let rgbString = box.style.backgroundColor
+                let rgb = rgbString.substring(4, rgbString.length - 1).split(", ");
+                let colorValue = getColorValue(rgb[0], rgb[1], rgb[2]);
+                if (colorValue != NaN) document.getElementById("picker").value = colorValue;
+                enterPopup();
+            })
             boxesContainer.append(box);
         }
     }
 }
 
-function getHex() {
+function getRGB(colorValue) {
+    let red;
+    let green;
+    let blue;
+
+    if (colorValue <= 50) {
+        red = 138+Math.floor(99*colorValue/50);
+        green = 186+Math.floor(8*colorValue/50);
+        blue = 211+Math.floor(5*colorValue/50);
+        return `rgb(${red}, ${green}, ${blue})`;
+    } else {
+        red = 237+Math.floor(15*(colorValue-50)/50);
+        green = 194+Math.floor(52*(colorValue-50)/50);
+        blue = 216+Math.floor(19*(colorValue-50)/50);
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
+}
+
+function getColorValue(red, green, blue) {
+    let fromRedElse = (red-237) *50/15 + 50
+    let fromGreenElse = (green-194) *50/52 + 50
+    let fromBlueElse = (blue-216) *50/19 + 50
+    let fromElse = (fromRedElse + fromGreenElse + fromBlueElse) / 3
+
+    let fromRed = (red-138) *50/99
+    let fromGreen = (green-186) *50/8
+    let fromBlue = (blue-211) *50/5
+    let from = (fromRed + fromGreen + fromBlue) / 3
+
+    if (from <= 50) return from;
+    return fromElse;
+}
+
+function exitPopup() {
+    popup.style.animation = "fadeout 0.15s linear forwards";
+    overlay.style.animation = "fadeout 0.15s linear forwards";
+    setTimeout(function() {
+        popup.style.display = "none";
+        overlay.style.display = "none";
+        document.getElementById("datePopup").valueAsDate = new Date(year, month, day);
+        document.getElementById('datePopup').style.marginBottom = "10px";
+        document.getElementById('error').style.display = "none";
+    }, 200);
+}
+
+function enterPopup() {
+    popup.style.display = "block";
+    overlay.style.display = "block";
+    popup.style.animation = "fadein 0.15s linear forwards";
+    overlay.style.animation = "fadein 0.15s linear forwards";
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    createBoard();
+
+    document.getElementById('addEntry').addEventListener('click',enterPopup);
+    document.getElementById('exit').addEventListener('click', exitPopup);
+    document.getElementById('overlay').addEventListener('click', exitPopup);
+
+    document.getElementById('reset').addEventListener('click', function() {
+        const dateInput = document.getElementById('datePopup');
+        const date = dateInput.value.split("-");
+        const currBox = document.getElementById(`${date[1] - 1}${date[2] - 1}`);
+        currBox.style.backgroundColor = "#cbc8c82e";
+        exitPopup();
+    })
     popup.addEventListener('submit', function(event) {
         event.preventDefault(); 
 
         // get value of picker
         const colorValue = document.getElementById("picker").value;
-        console.log(colorValue)
-        let hexColor;
+        let rgbColor = getRGB(colorValue);
 
-        let red;
-        let green;
-        let blue;
-
-        if (colorValue <= 50) {
-            red = 138+Math.floor(99*colorValue/50);
-            green = 186+Math.floor(8*colorValue/50);
-            blue = 211+Math.floor(5*colorValue/50);
-            hexColor = `rgb(${red}, ${green}, ${blue})`;
-        } else {
-            red = 237+Math.floor(15*(colorValue-50)/50);
-            green = 194+Math.floor(52*(colorValue-50)/50);
-            blue = 216+Math.floor(19*(colorValue-50)/50);
-            hexColor = `rgb(${red}, ${green}, ${blue})`;
-        }
-
-        // const hexInput = document.getElementById('picker');
-        // const hexInput = "c9ae61"; // temp
         const dateInput = document.getElementById('datePopup');
         const date = dateInput.value.split("-");
 
@@ -84,37 +138,9 @@ function getHex() {
         document.getElementById('error').style.display = "none";
         
         const currBox = document.getElementById(`${date[1] - 1}${date[2] - 1}`);
-        currBox.style.backgroundColor = hexColor;
-
-        // reverts back to today's date
-        document.getElementById("datePopup").valueAsDate = new Date(year, month, day);
+        currBox.style.backgroundColor = rgbColor;
 
         exitPopup();
     });
-}
 
-function exitPopup() {
-    popup.style.animation = "fadeout 0.15s linear forwards";
-    overlay.style.animation = "fadeout 0.15s linear forwards";
-    setTimeout(function() {
-        popup.style.display = "none";
-        overlay.style.display = "none";
-    }, 200);
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    createBoard();
-    getHex();
-
-    document.getElementById('addEntry').addEventListener('click', function() {
-        popup.style.display = "block";
-        overlay.style.display = "block";
-        popup.style.animation = "fadein 0.15s linear forwards";
-        overlay.style.animation = "fadein 0.15s linear forwards";
-    })
-
-    document.getElementById('exit').addEventListener('click', exitPopup);
-
-    document.getElementById('overlay').addEventListener('click', exitPopup);
 })
